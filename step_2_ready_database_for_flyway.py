6c4de8e9b937982b9aa6aa06054c994775f9d0fe
howarddeiner <howard.deiner@deinersoft.com>
2019-09-04 08:50:44 -0400

    Initial commit.

--- step_2_ready_database_for_flyway.py
+++ step_2_ready_database_for_flyway.py
@@ -3,10 +3,6 @@ import os
 
 os.system('figlet -w 160 -f standard "Ready Database for Flyway"')
 
-#os.system('echo "CREATE USER FLYWAY IDENTIFIED BY FLYWAY;" | sqlplus system/oracle@localhost:1521/EE > /dev/null')
-#os.system('echo "GRANT CREATE SESSION, CREATE ANY TABLE, CREATE ANY INDEX, CREATE ANY SEQUENCE, CREATE ANY VIEW, RESOURCE, DBA TO FLYWAY;" | sqlplus system/oracle@localhost:1521/EE > /dev/null')
-#os.system('echo "GRANT UNLIMITED TABLESPACE TO FLYWAY;" | sqlplus system/oracle@localhost:1521/EE > /dev/null')
-
-os.system('echo "CREATE USER FLYWAY IDENTIFIED BY FLYWAY;" | sqlplus system/oracle@localhost:1521/EE.oracle.docker')
-os.system('echo "GRANT CREATE SESSION, CREATE ANY TABLE, CREATE ANY INDEX, CREATE ANY SEQUENCE, CREATE ANY VIEW, RESOURCE, DBA TO FLYWAY;" | sqlplus system/oracle@localhost:1521/EE.oracle.docker')
-os.system('echo "GRANT UNLIMITED TABLESPACE TO FLYWAY;" | sqlplus system/oracle@localhost:1521/EE.oracle.docker')
+os.system('echo "CREATE USER FLYWAY IDENTIFIED BY FLYWAY;" | sqlplus system/oracle@localhost:1521/EE.oracle.docker > /dev/null')
+os.system('echo "GRANT CREATE SESSION, CREATE ANY TABLE, CREATE ANY INDEX, CREATE ANY SEQUENCE, CREATE ANY VIEW, RESOURCE, DBA TO FLYWAY;" | sqlplus system/oracle@localhost:1521/EE.oracle.docker > /dev/null')
+os.system('echo "GRANT UNLIMITED TABLESPACE TO FLYWAY;" | sqlplus system/oracle@localhost:1521/EE.oracle.docker > /dev/null')
